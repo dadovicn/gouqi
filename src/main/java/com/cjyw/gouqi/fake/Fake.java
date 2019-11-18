8abad69bc9afceba77b7c4bed0f35aafd3329cc9
dadovicn <dongfangdaodao@163.com>
2019-11-18 09:15:35 +0800

    feat: add angle data

--- src/main/java/com/cjyw/gouqi/fake/Fake.java
+++ src/main/java/com/cjyw/gouqi/fake/Fake.java
@@ -12,28 +12,30 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.UUID;
 
-
 /**
  * 模拟发射数据
  */
 public class Fake {
 
     public static void test () throws IOException, InterruptedException {
-        FileReader fr = new FileReader("/Users/duandongdong/daodao/open/gouqi/src/main/resources/ds/radar.txt");
+        FileReader fr = new FileReader("/Users/duandongdong/daodao/open/gouqi/src/main/resources/ds/all_radar-log.txt");
         BufferedReader br=new BufferedReader(fr);
         String line = "";
         Thread.sleep(20000);
         while ((line=br.readLine())!=null) {
             String tmp =line.split("-->")[1];
-            String canId = tmp.split(",")[0].split(":")[1];
-            String con = tmp.split(",")[1].split(":")[1];
-            String range = tmp.split(",")[2].split(":")[1];
-            String radar = (canId + ": " + con + ":" + range);
+
+            String canId = tmp.split(",")[1].split(":")[1];
+            String con = tmp.split(",")[4].split(":")[1];
+            String range = tmp.split(",")[6].split(":")[1].split("-")[0];
+            String angle = tmp.split(",")[5].split(":")[1].split("-")[0];
+            String v = tmp.split(",")[8].split(":")[1].split("-")[0];
+            String radar = (canId + ":" + con + ":" + range + ":" + angle + ":" + v);
             HashMap<UUID, SocketIOClient> userClient = ClientCache.getUserClient("dadovicn");
             userClient.forEach((uuid, socketIOClient) -> {
                 socketIOClient.sendEvent("dadovicn", radar);
             });
-            Thread.sleep(1000);
+            Thread.sleep(500);
         }
         br.close();
         fr.close();
@@ -53,6 +55,5 @@ public class Fake {
         server.start();
         System.out.println("启动正常");
         test();
-
     }
 }
