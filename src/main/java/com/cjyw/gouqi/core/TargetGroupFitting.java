ef9de5e47519576356ede76c4a0e6f59d2213135
dadovicn <dongfangdaodao@163.com>
2019-12-04 09:52:51 +0800

    feat: merge version

--- src/main/java/com/cjyw/gouqi/core/TargetGroupFitting.java
+++ src/main/java/com/cjyw/gouqi/core/TargetGroupFitting.java
@@ -9,24 +9,29 @@ import com.google.common.cache.CacheBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
-import static java.util.Map.*;
+import static java.util.Map.Entry;
 
 /**
  * 目标拟合算法
  */
-public class TargetFitting {
+public class TargetGroupFitting {
     private static final double  ANGLE_SPLIT = 0d;
-    private static final Logger log = LoggerFactory.getLogger(TargetFitting.class);
+    private static final Logger log = LoggerFactory.getLogger(TargetGroupFitting.class);
     private static AtomicInteger cartId = new AtomicInteger(0);
+    private static AtomicInteger carId = new AtomicInteger(0);
 
 
     /** <k, range> */
-    private static Cache<Integer, Double> fitMap = CacheBuilder.newBuilder()
+    private static Cache<Integer, Double> fitCartMap = CacheBuilder.newBuilder()
+            .maximumSize(32).expireAfterAccess(1000, TimeUnit.MILLISECONDS).build();
+
+    private static Cache<Integer, Double> fitCarMap = CacheBuilder.newBuilder()
             .maximumSize(32).expireAfterAccess(1000, TimeUnit.MILLISECONDS).build();
 
     /**
@@ -43,38 +48,52 @@ public class TargetFitting {
         }
         if(cur.getTraceVal().intValue() == 1 && cur.getRangeVal() < 60) {
             // 目标出现
-
-            if(fitMap.size() == 0) {
-                cartId.set(0);
-                fitMap.put(cartId.addAndGet(1), cur.getRangeVal());
-                // 第一个目标
-            } else {
-                if(checkTargetCart(cur.getRangeVal())) {
-                    // 新目标出现
-                    if(fitMap.asMap().size() >= 2) {
-                        launderTarget();
-                    }
-
-                    log.info("新目标出现: ", cartId.toString() );
-                    fitMap.put(cartId.addAndGet(1), cur.getRangeVal());
-                    if(fitMap.asMap().size() >= 2) {
-                        launderTarget();
-                    }
+            if(cur.getRangeVal().intValue() < 25) {
+                if(fitCartMap.size() == 0) {
+                    cartId.set(0);
+                    fitCartMap.put(cartId.addAndGet(1), cur.getRangeVal());
+                    // 第一个目标
                 } else {
-                    // 已有目标, 更新目标
-                    log.info("先找目标, 然后更新, 更新模式");
-                    updatePosition(cur.getRangeVal(), cur);
+                    if(checkTargetCart(cur.getRangeVal())) {
+                        // 新目标出现
+                        log.info("新目标出现: ", cartId.toString() );
+                        fitCartMap.put(cartId.addAndGet(1), cur.getRangeVal());
+                        if(fitCartMap.asMap().size() >= 2) {
+                            launderCartTarget();
+                        }
+                    } else {
+                        // 已有目标, 更新目标
+                        log.info("先找目标, 然后更新, 更新模式");
+                        updatePosition(cur.getRangeVal(), cur, fitCartMap);
+                    }
+                }
+            } else {
+                if(fitCarMap.size() == 0) {
+                    carId.set(50);
+                    fitCarMap.put(carId.addAndGet(1), cur.getRangeVal());
+                    // 第一个目标
+                } else {
+                    if(checkTarget(cur.getRangeVal(), fitCarMap)) {
+                        // 新目标出现
+                        log.info("新目标出现: ", carId.toString() );
+                        fitCarMap.put(carId.addAndGet(1), cur.getRangeVal());
+                        if(fitCarMap.asMap().size() >= 2) {
+                            launderCarTarget();
+                        }
+                    } else {
+                        // 已有目标, 更新目标
+                        log.info("先找目标, 然后更新, 更新模式");
+                        updatePosition(cur.getRangeVal(), cur, fitCarMap);
+                    }
                 }
             }
-
-
 
         }
         if(cur.getTraceVal().intValue() == 3) {
             // 更新距离
             // 已经被跟踪的目标
-            updatePosition(cur.getRangeVal(), cur);
-            updatePosition(cur.getRangeVal(), cur);
+            updatePosition(cur.getRangeVal(), cur, fitCarMap);
+            updatePosition(cur.getRangeVal(), cur, fitCartMap);
         }
     }
 
@@ -86,7 +105,7 @@ public class TargetFitting {
      * @return
      */
     public static boolean checkTargetCart(Double range) {
-        Optional<Entry<Integer, Double>> lastCur = fitMap.asMap().entrySet().stream().reduce((x, y) -> x.getValue() < y.getValue() ? x: y);
+        Optional<Entry<Integer, Double>> lastCur = fitCartMap.asMap().entrySet().stream().reduce((x, y) -> x.getValue() < y.getValue() ? x: y);
         if(lastCur.isPresent()) {
             if((range -lastCur.get().getValue()) > 20) {
                 return range < lastCur.get().getValue();
@@ -113,7 +132,7 @@ public class TargetFitting {
      * @param cur
      * @return
      */
-    public static void updatePosition(Double range, Target cur) {
+    public static void updatePosition(Double range, Target cur, Cache<Integer, Double> cache) {
         // 先找是属于哪边的车
 
         // 规约开始
@@ -121,23 +140,23 @@ public class TargetFitting {
             // 直接丢掉
             return;
         }
-        Optional<Entry<Integer, Double>> max = fitMap.asMap().entrySet().stream().reduce((x, y) -> x.getValue() < y.getValue() ? y: x);
+        Optional<Entry<Integer, Double>> max = cache.asMap().entrySet().stream().reduce((x, y) -> x.getValue() < y.getValue() ? y: x);
         if(max.isPresent()) {
             double maxRange = max.get().getValue();
             int pos = max.get().getKey();
             if(range > maxRange) {
                 cur.setCanId(pos);
                 log.info(cur.toString());
-                fitMap.put(pos, range);
+                cache.put(pos, range);
                 TargetReport.notifyCloud(PropertiesSource.INSTANCE.getConfig().rabbitConfig, JSON.toJSONString(cur));
             }
         }
-        fitMap.put(0, 0d);
-        fitMap.asMap().entrySet().stream().reduce((x, y) -> {
+        cache.put(0, 0d);
+        cache.asMap().entrySet().stream().reduce((x, y) -> {
             if (x.getValue() < range && range < y.getValue()) {
                 // 更新目标位置
                 if(x.getKey() != 0) {
-                    fitMap.put(x.getKey(), range);
+                    cache.put(x.getKey(), range);
                     cur.setCanId(x.getKey());
                     log.info(cur.toString());
                     TargetReport.notifyCloud(PropertiesSource.INSTANCE.getConfig().rabbitConfig, JSON.toJSONString(cur));
@@ -145,29 +164,41 @@ public class TargetFitting {
             }
             return y;
         });
-        fitMap.invalidate(0);
+        cache.invalidate(0);
     }
 
     /**
      * 目标清洗: 移除假目标
      */
-    public static void launderTarget() {
+    public static void launderCartTarget() {
         List<Integer> mqp = new ArrayList<>(10);
-        log.info("移除目标: {}", mqp.toString());
-        if(fitMap.asMap().size() > 2) {
-            fitMap.asMap().entrySet().stream().reduce((x, y) -> {
-                if(y.getValue() - x.getValue() < 5) {
+        if(fitCartMap.asMap().size() > 2) {
+            fitCartMap.asMap().entrySet().stream().reduce((x, y) -> {
+                if(y.getValue() - x.getValue() < 10) {
                     mqp.add(x.getKey());
                 }
                 return y;
             });
         }
         mqp.forEach(i -> {
-            fitMap.invalidate(i);
+            fitCartMap.invalidate(i);
         });
     }
 
-
+    public static void launderCarTarget() {
+        List<Integer> mqp = new ArrayList<>(10);
+        if(fitCarMap.asMap().size() > 2) {
+            fitCarMap.asMap().entrySet().stream().reduce((x, y) -> {
+                if(y.getValue() - x.getValue() < 20) {
+                    mqp.add(x.getKey());
+                }
+                return y;
+            });
+        }
+        mqp.forEach(i -> {
+            fitCarMap.invalidate(i);
+        });
+    }
 
     /**
      * todo 滤波
